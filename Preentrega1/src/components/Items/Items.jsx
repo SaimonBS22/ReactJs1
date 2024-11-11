e3e3b2c1b3f0ccaa5a96f03660d629bc0cdccb28
simon blaksley <simonblaksley@gmail.com>
2024-11-11 13:55:59 -0300

    agregando firebase

--- Preentrega1/src/components/Items/Items.jsx
+++ Preentrega1/src/components/Items/Items.jsx
@@ -3,11 +3,9 @@ function Items({item}){
         return(
            <div>
             <img src={item.thumbnail} className='imagenProducto' />
-            <h3>{item.title}</h3> 
+            <h3>{item.title}</h3>
             <Link to={`/item/${item.id}`}><button className='botonDetalles'>Ver Mas</button></Link>
            </div>
         )
 }
-
-
 export default Items
